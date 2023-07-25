c9841d9a91ba96fbc9bb8cd18a57fe30144faf7d
Artem <lobsterme@yandex.ru>
2023-07-25 14:51:19 +0300

    1

--- scripts/index.js
+++ scripts/index.js
@@ -1,148 +1,141 @@
-// Темплейт
-const itemTemplate = document.getElementById("item__template").content
+//формы
+const formProfile = document.querySelector("#editForm")
+const formProfileNew = document.querySelector("#addForm")
 
-// Попапы
-// новое место
-const placePopup = document.getElementById("popup_place")
-// редактирование профиля
-const profilePopup = document.getElementById("popup_profile")
-// большой картинки
-const picturePopup = document.getElementById("popup_picture")
+//попапы
+const popupEditProfile = document.querySelector(".popup_type_edit-profile")
+const popupAddCard = document.querySelector(".popup_type_add-card")
+const popupImage = document.querySelector(".popup_type_image")
 
-// Кнопки открытия попапов
-const editButtonProfile = document.querySelector(".profile__edit-button")
-const openPlacePopupButton = document.querySelector(".profile__button")
+//кнопки
+const profileAddButton = document.querySelector(".profile__add-button")
+const profileEditButton = document.querySelector(".profile__edit-button")
+const buttonClosePopupProfile = popupEditProfile.querySelector(".popup__close")
+const buttonClosePopupAddCard = popupAddCard.querySelector(".popup__close")
+const buttonClosePopupImage = popupImage.querySelector(".popup__close")
 
-// Все кнопки закрытия попапов
-const closePopupButtonProfile = profilePopup.querySelector(".popup__close")
-const closePopupButtonPlace = placePopup.querySelector(".popup__close")
-const closePopupButtonBigImage = picturePopup.querySelector(".popup__close")
+//поля инпутов
+const nameInput = popupEditProfile.querySelector(".popup__input_type_name")
+const jobInput = popupEditProfile.querySelector(".popup__input_type_job")
 
-// Все попапы сохранены в одной
-// переменной popups(множественное число)
-const popups = document.querySelectorAll(".popup")
+//профиль тайтл и сабтайтл
+const profileTitle = document.querySelector(".profile__title")
+const profileParag = document.querySelector(".profile__subtitle")
 
-// Формы
-const profileForm = document.querySelector(".popup__form_form")
-const placeForm = document.getElementById("place__form")
+const cardsContainer = document.querySelector(".elements")
+const template = document.querySelector("#element-template").content
 
-// Поля форм по их name в конкретной форме html разметки
-// поля редактирования попапа
-const nameValue = profileForm.elements.name
-const jobValue = profileForm.elements.job
+const imageOpenPopup = document.querySelector(".popup__image-container")
+const imageClosePopup = document.querySelector(".popup__close")
+const imageImg = document.querySelector(".popup__image")
+const imageTitle = document.querySelector(".popup__image-title")
 
-// поля попапа новое место
-const titleplaceValue = placeForm.elements.name
-const linkPlaceValue = placeForm.elements.link
+const nameInputNew = document.querySelector(".popup__input_type_image-name")
+const linkInputNew = document.querySelector(".popup__input_type_image-link")
 
-// Константы
-const nameInputContent = document.querySelector(".profile__title")
-const jobInputContent = document.querySelector(".profile__subtitle")
-const cardsAddContainer = document.querySelector(".elements__list")
-const titlePicturePopup = document.querySelector(".popup__title-picture")
-const elementTitle = document.querySelector(".element__title")
-const imgPicturePopup = document.querySelector(".popup__image-picture")
-
-/////////////////////
-///     ФУНКЦИИ   ///
-/////////////////////
-
+//функция открытия попапов
 function openPopup(popup) {
+  document.addEventListener("keydown", closePopupOnEscape)
   popup.classList.add("popup_opened")
 }
 
+function closePopupOnEscape(evt) {
+  if (evt.code == "Escape") {
+    const popup = document.querySelector(".popup_opened")
+    closePopup(popup)
+  }
+}
+
+//функция закрытия попапов
 function closePopup(popup) {
   popup.classList.remove("popup_opened")
+  document.removeEventListener("keydown", closePopupOnEscape)
 }
 
-// попап открытия редактирования профиля и присвоение полей с данными
-function openPopupFormProfile() {
-  openPopup(profilePopup)
-  nameValue.value = nameInputContent.textContent
-  jobValue.value = jobInputContent.textContent
-}
-
-// отправка формы редактирования данных профиля
-function handleProfileFormSubmit(event) {
-  event.preventDefault()
-  nameInputContent.textContent = nameValue.value
-  jobInputContent.textContent = jobValue.value
-  closePopup(profilePopup)
-}
-
-// открываю попап с местами (добавление карточек)
-function openPopupPlace() {
-  openPopup(placePopup)
-}
-
-/////////////////////////////////
-///     ОБРАБОТЧИКИ КЛИКОВ   ///
-///////////////////////////////
-
-editButtonProfile.addEventListener("click", openPopupFormProfile)
-
-openPlacePopupButton.addEventListener("click", openPopupPlace)
-
-profileForm.addEventListener("submit", handleProfileFormSubmit)
-
-placeForm.addEventListener("submit", handlePlaceFormSubmit)
-
-popups.forEach((popup) => {
-  popup.addEventListener("click", (evt) => {
-    if (evt.currentTarget === evt.target) closePopup(popup)
-  })
+profileEditButton.addEventListener("click", () => {
+  nameInput.value = profileTitle.textContent
+  jobInput.value = profileParag.textContent
+  disableSubmitButton(popupEditProfile, validationConfig)
+  openPopup(popupEditProfile)
 })
 
-//закрытие попапа при клике на крестик для имени и о себе
-closePopupButtonProfile.addEventListener("click", () => {
-  closePopup(profilePopup)
+profileAddButton.addEventListener("click", () => {
+  disableSubmitButton(popupAddCard, validationConfig)
+  openPopup(popupAddCard)
 })
 
-//закрытия попапа добавления карточки
-closePopupButtonPlace.addEventListener("click", () => {
-  closePopup(placePopup)
+buttonClosePopupProfile.addEventListener("click", () => {
+  closePopup(popupEditProfile)
 })
 
-//закрытие попапа большой картинки
-closePopupButtonBigImage.addEventListener("click", () => {
-  closePopup(picturePopup)
+buttonClosePopupAddCard.addEventListener("click", () => {
+  closePopup(popupAddCard)
 })
 
-/////////////////////
-///     ФУНКЦИИ   ///
-/////////////////////
+buttonClosePopupImage.addEventListener("click", () => {
+  closePopup(popupImage)
+})
 
-function openPicturePopup(name, link) {
-  picturePopup.classList.add("popup_opened")
-  imgPicturePopup.src = link
-  imgPicturePopup.alt = name
-  titlePicturePopup.textContent = name
-  openPopup(picturePopup)
+popupEditProfile.addEventListener("click", (evt) => {
+  if (evt.currentTarget === evt.target) {
+    closePopup(popupEditProfile)
+  }
+})
+
+popupAddCard.addEventListener("click", (evt) => {
+  if (evt.currentTarget === evt.target) {
+    closePopup(popupAddCard)
+  }
+})
+
+popupImage.addEventListener("click", (evt) => {
+  if (evt.currentTarget === evt.target) {
+    closePopup(popupImage)
+  }
+})
+
+function submitProfileForm(evt) {
+  evt.preventDefault()
+  profileTitle.textContent = nameInput.value
+  profileParag.textContent = jobInput.value
+  closePopup(popupEditProfile)
 }
 
-function generateCard(value) {
-  //клонирую карточку
-  const card = itemTemplate.querySelector(".element").cloneNode(true)
+formProfile.addEventListener("submit", submitProfileForm)
+
+function imageOpen(card, link) {
+  imageImg.src = link
+  imageImg.alt = card
+  imageTitle.textContent = card
+  openPopup(popupImage)
+}
+
+function createCard(value) {
+  const card = template.querySelector(".element").cloneNode(true)
   if (card) {
     const title = card.querySelector(".element__title")
-    const img = card.querySelector(".element__img")
+    const mask = card.querySelector(".element__mask")
     const trash = card.querySelector(".element__trash")
-    const like = card.querySelector(".element__like")
-
-    if (title && img && trash && like) {
-      //заполняю контентом
+    const like = card.querySelector(".element__like-button")
+    if (title && mask && trash && like) {
       title.textContent = value.name
-      img.src = value.link
-      img.alt = value.name
-
-      like.addEventListener("click", toggleLike)
-
+      mask.src = value.link
+      mask.alt = title.textContent
+      mask.addEventListener("click", () => {
+        imageOpen(title.textContent, value.link)
+      })
       trash.addEventListener("click", () => {
         card.remove()
       })
 
-      img.addEventListener("click", () => {
-        openPicturePopup(value.name, value.link)
+      like.addEventListener("click", () => {
+        const className = "element__like-button_active"
+        if (like)
+          if (like.classList.contains(className)) {
+            like.classList.remove(className)
+          } else {
+            like.classList.add(className)
+          }
       })
     }
   }
@@ -154,24 +147,22 @@ function renderCard(card, container) {
 }
 
 function render() {
-  initialCards.forEach((value) => {
-    const newCard = generateCard(value)
-    if (newCard) renderCard(newCard, cardsAddContainer)
+  initialCards.reverse().forEach((value) => {
+    const newCard = createCard(value)
+    if (newCard) renderCard(newCard, cardsContainer)
   })
 }
 
 render()
 
-function toggleLike(event) {
-  event.target.classList.toggle("element__like_active")
+function submitCardForm(evt) {
+  evt.preventDefault()
+  const name = nameInputNew.value
+  const link = linkInputNew.value
+  const newCard = createCard({ name, link })
+  if (newCard) renderCard(newCard, cardsContainer)
+  closePopup(popupAddCard)
+  formProfileNew.reset()
 }
 
-function handlePlaceFormSubmit(evt) {
-  evt.preventDefault()
-  const name = titleplaceValue.value
-  const link = linkPlaceValue.value
-  const newCard = generateCard({ name, link })
-  if (newCard) renderCard(newCard, cardsAddContainer)
-  closePopup(placePopup)
-  placeForm.reset()
-}
+formProfileNew.addEventListener("submit", submitCardForm)
